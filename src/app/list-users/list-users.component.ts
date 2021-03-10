2a42f0f71a2bea5732abbb4c9a1db2a3a9f4567d
ayyoubelkanaany <elkanaany.21@gmail.com>
2021-03-10 12:07:32 +0100

    filter work

--- src/app/list-users/list-users.component.ts
+++ src/app/list-users/list-users.component.ts
@@ -10,16 +10,27 @@ import {Router} from '@angular/router';
 })
 export class ListUsersComponent implements OnInit {
   users: Array<User>;
+  usersRef: Array<User>;
   constructor(private userService: UserService, private route: Router) {
     this.users = new Array<User>();
+    this.usersRef = new Array<User>();
   }
 
   ngOnInit(): void {
     this.userService.getUsers().subscribe(data => {
       this.users = data;
+      this.usersRef = data;
     });
   }
   getDetails(id: number){
     this.route.navigate(['details']);
+  }
+  filter(criter: string){
+    if (criter !== ''){
+      this.users = this.users.filter(value => value.name.startsWith(criter));
+    }
+    else {
+      this.users = this.usersRef;
+    }
   }
 }
